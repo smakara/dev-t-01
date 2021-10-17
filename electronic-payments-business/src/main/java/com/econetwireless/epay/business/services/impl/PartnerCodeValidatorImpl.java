54828fe1d89c62091591fb1d4f012628d145a836
smakara <shepherdmakara@gmail.com>
2021-10-17 07:43:08 +0200

    sort the super key word in PartnerCodeValidatorImpl class

--- electronic-payments-business/src/main/java/com/econetwireless/epay/business/services/impl/PartnerCodeValidatorImpl.java
+++ electronic-payments-business/src/main/java/com/econetwireless/epay/business/services/impl/PartnerCodeValidatorImpl.java
@@ -8,21 +8,21 @@ import com.econetwireless.utils.execeptions.EpayException;
 /**
  * Created by tnyamakura on 18/3/2017.
  */
-public class PartnerCodeValidatorImpl implements PartnerCodeValidator{
+public class PartnerCodeValidatorImpl implements PartnerCodeValidator {
 
-    private RequestPartnerDao requestPartnerDao;
+	private RequestPartnerDao requestPartnerDao;
 
-    public PartnerCodeValidatorImpl(RequestPartnerDao requestPartnerDao) {
-        this(super);
-        this.requestPartnerDao = requestPartnerDao;
-    }
+	public PartnerCodeValidatorImpl(RequestPartnerDao requestPartnerDao) {
+		super();
+		this.requestPartnerDao = requestPartnerDao;
+	}
 
-    @Override
-    public boolean validatePartnerCode(final String partnerCode) {
-        final boolean isValidPartner = requestPartnerDao.findByCode(partnerCode) != null;
-        if(!isValidPartner) {
-            throw new EpayException(ResponseCode.INVALID_REQUEST, "Invalid partner code supplied : "+partnerCode);
-        }
-        return isValidPartner;
-    }
+	@Override
+	public boolean validatePartnerCode(final String partnerCode) {
+		final boolean isValidPartner = requestPartnerDao.findByCode(partnerCode) != null;
+		if (!isValidPartner) {
+			throw new EpayException(ResponseCode.INVALID_REQUEST, "Invalid partner code supplied : " + partnerCode);
+		}
+		return isValidPartner;
+	}
 }

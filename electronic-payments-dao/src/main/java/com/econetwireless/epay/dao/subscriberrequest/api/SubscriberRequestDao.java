88bb7c07c9f7bfb77c65793a16890c161a5b1585
smakara <shepherdmakara@gmail.com>
2021-10-17 07:51:26 +0200

    1.added the following  methods in interface =>:SubscriberRequestDao
    SubscriberRequest persist(SubscriberRequest subscriberRequest);
    void update(SubscriberRequest createdSubscriberRequest);

--- electronic-payments-dao/src/main/java/com/econetwireless/epay/dao/subscriberrequest/api/SubscriberRequestDao.java
+++ electronic-payments-dao/src/main/java/com/econetwireless/epay/dao/subscriberrequest/api/SubscriberRequestDao.java
@@ -1,14 +1,19 @@
 package com.econetwireless.epay.dao.subscriberrequest.api;
 
-import com.econetwireless.epay.domain.SubscriberRequest;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
+import com.econetwireless.epay.domain.SubscriberRequest;
 
 /**
  * Created by tnyamakura on 17/3/2017.
  */
 public interface SubscriberRequestDao extends JpaRepository<SubscriberRequest, Long> {
-    List<SubscriberRequest> findByPartnerCode(@Param("partnerCode") String partnerCode);
+	List<SubscriberRequest> findByPartnerCode(@Param("partnerCode") String partnerCode);
+
+	SubscriberRequest persist(SubscriberRequest subscriberRequest);
+
+	void update(SubscriberRequest createdSubscriberRequest);
 }
